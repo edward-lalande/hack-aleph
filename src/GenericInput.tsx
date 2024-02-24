8c8d5b08b72f435f5614c4faf758698460f807b1
EdwZak <edward.lalande@epitech.eu>
2024-02-24 05:23:51 +0100

    feats: front, chat pages

---
+++ src/GenericInput.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Box, TextField, InputAdornment, IconButton } from '@mui/material';
+
+interface GenericInputProps {
+    labelName: string;
+    icon: any;
+}
+
+const GenericInput: React.FC<GenericInputProps> = ({ labelName, icon }) => (
+    <TextField
+        label={labelName}
+        variant="outlined"
+        sx={{
+            marginTop: 'auto',
+            // border: "4px solid",
+            borderRadius: '50px',
+            '& .MuiOutlinedInput-notchedOutline': {
+                borderColor: '#508691',
+            },
+            '&:hover .MuiOutlinedInput-notchedOutline': {
+                borderColor: '#508691',
+            },
+            '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+                borderColor: '#508691',
+            },
+        }}
+        InputProps={{
+            endAdornment: (
+                <InputAdornment position="end">
+                    <IconButton>
+                        {icon}
+                    </IconButton>
+                </InputAdornment>
+            ),
+        }}
+    />
+);
+
+export default GenericInput;
--- src/HomeChat.tsx
+++ src/HomeChat.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
-import { Box, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Box, Button } from '@mui/material';
+import { Search } from '@mui/icons-material';
+import SendIcon from '@mui/icons-material/Send';
+import GenericInput from './GenericInput';
 
 const HomeChat: React.FC = () => {
+    const folders = Array.from({ length: 25 });
+    const [clickedFolders, setClickedFolders] = useState<boolean[]>(Array(30).fill(false));
+
+    const handleFolderClick = (index: number) => {
+        const newClickedFolders = [...clickedFolders];
+        newClickedFolders[index] = !newClickedFolders[index];
+        setClickedFolders(newClickedFolders);
+    };
+    
     return (
         <Box sx={{
             display: "flex",
@@ -11,7 +23,42 @@ const HomeChat: React.FC = () => {
             width: "80vw",
             borderTopRightRadius: "40px",
             borderBottomRightRadius: "40px",
+            overflow: "hidden",
         }}>
+            <Box sx={{
+                padding: "5%",
+                overflow: "hidden",
+            }}>
+                <GenericInput labelName="Search" icon={<Search />} />
+                <Box sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    flexWrap: "wrap",
+                    alignItems: "flex-start",
+                    border: "solid 2px #508691",
+                    borderRadius: "30px",
+                    marginTop: "30px",
+                    height: "45vh",
+                    width: "65vw",
+                    overflow: "hidden",
+                }}>
+                    {folders.map((_, index) => (
+                        <Button
+                            onClick={() => handleFolderClick(index)}
+                            key={index}
+                            sx={{
+                                border: "solid 2px " + (clickedFolders[index] ? "black" : "#508691"),
+                                borderRadius: "30px",
+                                margin: "15px",
+                                height: "10vh",
+                                width: "5vw",
+                            }}>
+                            Perso
+                        </Button>
+                    ))}
+                </Box>
+            </Box>
+            <GenericInput labelName="Chat" icon={<SendIcon />} />
         </Box>
     );
 };
