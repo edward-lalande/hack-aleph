d52e1ae24e42a4406415199368bd0bd5f809f84a
Paul Bardeur <paul.bardeur@epitech.eu>
2024-02-24 17:49:18 +0100

    feat: llm algo

--- backend/llm.py
+++ backend/llm.py
@@ -4,7 +4,7 @@ from langchain.callbacks.manager import CallbackManagerForLLMRun
 from langchain.llms.base import LLM
 import requests
 
-API_BASE = "https://curated.aleph.cloud/vm/a8b6d895cfe757d4bc5db9ba30675b5031fe3189a99a14f13d5210c473220caf"
+API_BASE = "https://curated.aleph.cloud/vm/cb6a4ae6bf93599b646aa54d4639152d6ea73eedc709ca547697c56608101fc7"
 
 class Libertai(LLM):
     api_base: str = API_BASE
@@ -12,7 +12,7 @@ class Libertai(LLM):
     top_p: float = 1
     top_k: int = 40
     cache_prompt: bool = True
-    max_length: int = 400
+    max_length: int = 200
     slot_id: int = -1
     session: requests.Session = None
     stop: list = ["<|"]
@@ -28,9 +28,14 @@ class Libertai(LLM):
         run_manager: Optional[CallbackManagerForLLMRun] = None,
         **kwargs: Any,
     ) -> str:
-        """Call the LLM."""
+
         if self.session is None:
             self.session = requests.Session()
+
+        cookies = {'m1': 's1', 'm2': 's1', 'm3': 's1', 'm4': 's1', 'm5': 's1'}
+        self.session.cookies.update(cookies)
+        
+        print(prompt)
 
         params = {
             "prompt": prompt,
@@ -43,8 +48,9 @@ class Libertai(LLM):
             "slot_id": self.slot_id,
             "cache_prompt": True
         }
-
-        response = requests.post(f"{self.api_base}/completion", json=params)
+        
+        response = self.session.post(f"{self.api_base}/completion", json=params)
+    
         if response.status_code == 200:
             output = response.json()
             self.slot_id = output['slot_id']
--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,49 @@
 import llm
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain.embeddings import huggingface
 
-message = llm.openhermes._call("User: Qu'est ce que le C ?\n\nAssistant: ")
-print(message)
+llm = llm.openhermes
+
+template = """
+    Tu es un assistant virtuel qui doit répondre à une question.
+    Si tu ne sais pas, dis simplement que tu ne sais pas.
+    Si tu utilises de la 'Documentation', cites le fichier source et la page après avoir fait ta 'Réponse'.
+    Réponds dans la même langue que la question.
+
+    Documentation:
+    ###
+    {documents}
+    ###
+    
+    Question: 
+    ###
+    {input}
+    ###
+
+    Réponse:"""
+
+query = "Qui est Timothée BIGE ?"
+
+loader = PyPDFLoader("pv.pdf")
+docs = loader.load()
+
+text_splitter = CharacterTextSplitter(chunk_size=120, chunk_overlap=0, separator=". ")
+texts = text_splitter.split_documents(docs)
+
+embeddings = huggingface.HuggingFaceEmbeddings()
+db = Chroma.from_documents(texts, embeddings)
+
+retriever = db.as_retriever(search_kwargs={"k": 4})
+
+documents = retriever.get_relevant_documents(query)
+
+clean_docs = "\n\n".join(
+    [f"Document:\n" + (str)(documents[i].page_content).replace('\n', ' ') + "\nSources: " + documents[i].metadata["source"] + " page " + (str)(documents[i].metadata["page"]) for i, d in enumerate(documents)]
+)
+
+prompt = template.format(input=query, documents=clean_docs)
+answer = llm.predict(prompt)
+
+print(answer)
